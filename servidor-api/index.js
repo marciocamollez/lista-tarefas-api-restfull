52e575661f49b1abc434c4a56bad4c6c0c8e1147
Marcio <marciocamollez@hotmail.com>
2021-05-14 10:19:58 -0300

    first commit

---
+++ servidor-api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {
+    listarTarefaId,
+    listarTarefas,
+    cadastrarTarefa,
+    atualizarTarefa,
+    removerTarefa,
+    concluirTarefa
+  } = require('./controllers/gerenciador-tarefas.js');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// listar todas as tarefas - get
+app.get('/gerenciador-tarefas', listarTarefas);
+// listar uma tarefa por id - get
+app.get('/gerenciador-tarefas/:id', listarTarefaId);
+// cadastrar uma tarefa - post
+app.post('/gerenciador-tarefas', cadastrarTarefa);
+// atualizar uma tarefa - put
+app.put('/gerenciador-tarefas/:id', atualizarTarefa);
+// remover uma tarefa - delete
+app.delete('/gerenciador-tarefas/:id', removerTarefa);
+// concluir uma tarefa - put
+app.put('/gerenciador-tarefas/:id/concluir', concluirTarefa);
+
+
+app.listen(port, () => console.log('Servidor iniciado'));
